c2fbb2504fb3c9a256227000b75b020af88f02e8
aamircodes <aamirp1@outlook.com>
2022-02-16 14:48:34 +0000

    passes third test changing position of input value

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -2,31 +2,52 @@ require 'rspec'
 require './tictactoe'
 
 describe Tictactoe do
-    tictactoe = described_class.new
+
+
 
     it "outputs an empty board" do
 
-    # Act
-    position = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-    expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
-    result = tictactoe.display_board()
+        # Act
+        tictactoe = described_class.new
 
-    # Assert
-    expect(result).to eq(expected_board)
+        position = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
+        result = tictactoe.display_board()
+
+        # Assert
+        expect(result).to eq(expected_board)
 
     end
 
-    it "outputs board with an 'X' in the first turn" do
+    it "outputs board with an 'X' in the first turn at position [0][0]" do
 
-    # Act
-    position = [["X", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-    expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
+        # Act
+        tictactoe = described_class.new
+
+        position = [["X", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
+        
+        tictactoe.update_board(0,0,'X')
+        result = tictactoe.display_board()
+
+        # Assert
+        expect(result).to eq(expected_board)
     
-    tictactoe.update_board(0,0,'X')
-    result = tictactoe.display_board()
+    end
 
-    # Assert
-    expect(result).to eq(expected_board)
+    it "outputs board with an 'X' in the [0][1] position of board" do
+
+        # Act
+        tictactoe = described_class.new
+        
+        position = [[" ", "X", " "], [" ", " ", " "], [" ", " ", " "]]
+        expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
+        
+        tictactoe.update_board(0,1,'X')
+        result = tictactoe.display_board()
+
+        # Assert
+        expect(result).to eq(expected_board)
     
     end
 end
