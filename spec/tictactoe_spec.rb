b17940a808c78987df82bb80da593bfb19809a6f
aamircodes <aamirp1@outlook.com>
2022-02-16 14:16:52 +0000

    updated display_board function to pass initial test

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -2,19 +2,20 @@ require 'rspec'
 require './tictactoe'
 
 describe Tictactoe do
+
     it "outputs an empty board" do
 
     # Arrange
     tictactoe = described_class.new
 
     # Act
-    position = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[4]} | #{position[5]} | #{position[6]} ",  "-----------", " #{position[7]} | #{position[8]} | #{position[9]} "]
-
-    result = tictactoe.display_board("#(board}")
+    position = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    expected_board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
+    result = tictactoe.display_board()
 
     # Assert
-    expect(result).to eq(board)
+    expect(result).to eq(expected_board)
 
     end
+    
 end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,6 +1,12 @@
 class Tictactoe
-    def display_board(board)
-        position = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-        board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[4]} | #{position[5]} | #{position[6]} ",  "-----------", " #{position[7]} | #{position[8]} | #{position[9]} "]
+    attr_accessor :position
+
+    def initialize()
+        @position = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    end    
+
+    def display_board()
+        board = [" #{position[0]} | #{position[1]} | #{position[2]} ",  "-----------",  " #{position[3]} | #{position[4]} | #{position[5]} ",  "-----------", " #{position[6]} | #{position[7]} | #{position[8]} "]
     end
 end
+
