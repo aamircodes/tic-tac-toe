e30d274a965c491467c16605dddcbe8f9b7ad8d7
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2022-02-22 14:07:50 +0000

    Testing collab

--- tictactoe.rb
+++ tictactoe.rb
@@ -48,5 +48,11 @@ class Tictactoe
         board = [" #{position[0][0]} | #{position[0][1]} | #{position[0][2]} ",  "-----------",  " #{position[1][0]} | #{position[1][1]} | #{position[1][2]} ",  "-----------", " #{position[2][0]} | #{position[2][1]} | #{position[2][2]} "]
     end
 
+
+
+
+
+    
+
 end
 
